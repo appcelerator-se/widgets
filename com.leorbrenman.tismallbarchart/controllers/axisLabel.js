ddbf5e53049f0d0ffa04ccec3de4872ff0d5547e
Bert Grantges <bgrantges@appcelerator.com>
2014-05-07 09:51:40 -0500

    adding some missing files

---
+++ com.leorbrenman.tismallbarchart/controllers/axisLabel.js
@@ -0,0 +1,10 @@
+var _args = arguments[0] || {};
+
+// Ti.API.info("_args.text = "+_args.text);
+
+$.axisLabel.text = _args.text || null;
+$.axisLabel.bottom = _args.bottom || null;
+$.axisLabel.top = _args.top || null;
+$.axisLabel.right = _args.right || null;
+$.axisLabel.left = _args.left || null;
+$.axisLabel.color = _args.color || null;
