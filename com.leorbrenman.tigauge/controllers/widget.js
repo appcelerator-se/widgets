0da7d25a5915f556724c39a358514e4b553c10bc
Bert Grantges <bgrantges@appcelerator.com>
2014-05-07 08:39:16 -0500

    updates for Leors widgets 1

--- com.leorbrenman.tigauge/controllers/widget.js
+++ com.leorbrenman.tigauge/controllers/widget.js
@@ -1,30 +1,142 @@
 var _args = arguments[0] || {};
 
-Ti.API.info("_args.percent = "+_args.percent);
+var _percent="0";
+var _isAnimationOn = true;
+var defaultPercent = "50";
+var defaultGaugeHeight = "25";
 
-_args.percent ? $.tiGaugeMeterVw.right = (100-parseInt(_args.percent)).toString()+"%" : $.tiGaugeMeterVw.right = "50%";
+// Ti.API.info("_args.percent = "+_args.percent);
 
 $.tiGaugeMeterVw.backgroundColor = _args.gaugeColor || "#4ba90a";
-$.tiGaugeOverlayLbl.text = _args.overlayText || $.tiGaugeMeterVw.right;
+$.tiGaugeOverlayLbl.text = _args.overlayText || "";
 $.tiGaugeOverlayLbl.color = _args.overlayTextColor || "white";
 $.tiGaugeBGVw.backgroundColor =  _args.gaugeBGColor || "#e5e5e5";
+$.tiGaugeBGVw.height = _args.gaugeHeight || defaultGaugeHeight;
+$.tiGaugeMeterVw.height = _args.gaugeHeight || defaultGaugeHeight;
+$.tiGaugeTitleLbl.text = _args.titleText || null;
+$.tiGaugeTitleRightLbl.text = _args.titleRightText || null;
+_isAnimationOn = _args.isAnimationOn || true;
 
 $.setPercent = function (percent) {
-	$.tiGaugeMeterVw.right = (100-parseInt(percent)).toString()+"%";
+	_percent = percent;
+	
+	$.tiGaugeMeterVw.right = "99%";
+	
+	if(_isAnimationOn && OS_IOS) {
+		var animation = Titanium.UI.createAnimation();
+		animation.right = (100-parseInt(percent)).toString()+"%";
+		animation.duration = 1000;
+		$.tiGaugeMeterVw.animate(animation);	
+	} else {
+		$.tiGaugeMeterVw.right = (100-parseInt(percent)).toString()+"%";
+	}
+	
+	if($.tiGaugeTitleLbl.text || $.tiGaugeTitleRightLbl.text) {
+		$.tiGaugeTitleVw.height=Ti.UI.SIZE;
+	} else {
+		$.tiGaugeTitleVw.height="0";
+	}
+};
+
+$.getPercent = function () {
+	return _percent;
 };
 
 $.setGaugeBackgroundColor = function (backgroundColor) {
 	$.tiGaugeBGVw.backgroundColor = backgroundColor;
+	$.setPercent(_percent);
+};
+
+$.getGaugeBackgroundColor = function () {
+	return $.tiGaugeBGVw.backgroundColor;
 };
 
 $.setGaugeColor = function (backgroundColor) {
 	$.tiGaugeMeterVw.backgroundColor = backgroundColor;
+	$.setPercent(_percent);
+};
+
+$.getGaugeColor = function () {
+	return $.tiGaugeMeterVw.backgroundColor;
 };
 
 $.setOverlayText = function (overlayText) {
 	$.tiGaugeOverlayLbl.text = overlayText;
+	$.setPercent(_percent);
+};
+
+$.clearOverlayText = function () {
+	$.tiGaugeOverlayLbl.text = "";
+	$.setPercent(_percent);
+};
+
+$.getOverlayText = function () {
+	return $.tiGaugeOverlayLbl.text;
 };
 
 $.setOverlayTextColor = function (overlayTextColor) {
 	$.tiGaugeOverlayLbl.color = overlayColor;
+	$.setPercent(_percent);
 };
+
+$.getOverlayTextColor = function () {
+	return $.tiGaugeOverlayLbl.color;
+};
+
+$.enableAnimation = function () {
+	_isAnimationOn = true;
+	$.setPercent(_percent);
+};
+
+$.disableAnimation = function () {
+	_isAnimationOn = false;
+	$.setPercent(_percent);
+};
+
+$.getAnimationSetting = function () {
+	return _isAnimationOn;
+};
+
+$.setGaugeHeight = function (height) {
+	$.tiGaugeBGVw.height = height;
+	$.tiGaugeMeterVw.height = height;
+	$.setPercent(_percent);
+};
+
+$.getGaugeHeight = function () {
+	return $.tiGaugeBGVw.height;
+};
+
+$.setTitle = function (titleText) {
+	$.tiGaugeTitleLbl.text = titleText;
+	$.setPercent(_percent);
+};
+
+$.getTitle = function () {
+	return $.tiGaugeTitleLbl.text;
+};
+
+$.clearTitle = function () {
+	$.tiGaugeTitleLbl.text = null;
+	$.setPercent(_percent);
+};
+
+$.setTitleRight = function (titleRightText) {
+	$.tiGaugeTitleRightLbl.text = titleRightText;
+	$.setPercent(_percent);
+};
+
+$.clearTitleRight = function () {
+	$.tiGaugeTitleRightLbl.text = null;
+	$.setPercent(_percent);
+};
+
+$.getTitleRight = function () {
+	return $.tiGaugeTitleRightLbl.text;
+};
+
+if(_args.percent) {
+	$.setPercent(_args.percent);
+} else {
+	$.setPercent(defaultPercent);
+}
--- com.leorbrenman.tismallbarchart/controllers/widget.js
+++ com.leorbrenman.tismallbarchart/controllers/widget.js
@@ -5,70 +5,65 @@ var defaultData = [{"month": "1", "val": 2}, {"month": "2", "val": 2}, {"month":
 var defaultNegativeColor = "red";
 var defaultFontSize = 14;
 var _fontSize = defaultFontSize;
+var legendOn = false;
+var spacersOn = false;
 
-Ti.API.info("_args.height = "+_args.height);
-Ti.API.info("_args.width = "+_args.width);
-Ti.API.info("_args.negativeColor = "+_args.negativeColor);
-Ti.API.info("_args.displayLegend = "+_args.displayLegend);
-Ti.API.info("_args.displaySpacers = "+_args.displaySpacers);
-Ti.API.info("_args.data = "+_args.data);
+// Ti.API.info("_args.height = "+_args.height);
+// Ti.API.info("_args.width = "+_args.width);
+// Ti.API.info("_args.negativeColor = "+_args.negativeColor);
+// Ti.API.info("_args.displayLegend = "+_args.displayLegend);
+// Ti.API.info("_args.displaySpacers = "+_args.displaySpacers);
+// Ti.API.info("_args.data = "+_args.data);
 
 $.tiChartVw.height = _args.height || Ti.UI.FILL;
 $.tiChartVw.width = _args.width || Ti.UI.FILL;
 var negativeColor = _args.negativeColor || defaultNegativeColor;
-var legendOn = _args.displayLegend || false;
-var spacersOn = _args.displaySpacers || false;
-var colors = _args.colors || defaultColors;
+// var colors = _args.colors || defaultColors;
 var data = _args.data || defaultData;
 var labelColor = _args.labelColor || "black";
+var chartTitle = _args.chartTitle || null;
 
-setData(data);
+var colors;
 
-Ti.API.info("$.tiChartVw.height = "+$.tiChartVw.height);
-Ti.API.info("$.tiChartVw.width = "+$.tiChartVw.width);
-Ti.API.info("negativeColor = "+negativeColor);
-Ti.API.info("legendOn = "+legendOn);
-Ti.API.info("spacersOn = "+spacersOn);
+// process color as string or array of strings
+if(_args.colors) {
+	if(typeof _args.colors === "string") {
+		colors = [_args.colors];
+	}  else {
+		colors = _args.colors;
+	}
+} else {
+	colors = defaultColors;
+}
 
-$.setLegendOnStatus = function (_legendOnStatus) {
-	Ti.API.info("setLegendOnStatus()");
-	Ti.API.info("_legendOnStatus = "+_legendOnStatus); 
-	
-	legendOn = _legendOnStatus;
+// Ti.API.info("$.tiChartVw.height = "+$.tiChartVw.height);
+// Ti.API.info("$.tiChartVw.width = "+$.tiChartVw.width);
+// Ti.API.info("negativeColor = "+negativeColor);
+// Ti.API.info("legendOn = "+legendOn);
+// Ti.API.info("spacersOn = "+spacersOn);
+
+function clearChartView(){
+	_.each($.barChartVw.children, function(view) {
+		$.barChartVw.remove(view);
+	});
+	_.each($.barChartYLegendVw.children, function(view) {
+		$.barChartYLegendVw.remove(view);
+	});
+	_.each($.barChartXLegendVw.children, function(view) {
+		$.barChartXLegendVw.remove(view);
+	});
 };
 
-$.setColors = function (_colors) {
-	Ti.API.info("setColors()");
-	Ti.API.info("_colors = "+_colors); 
-	
-	colors = _colors;
-};
-
-$.setNegativeColor = function (_negativeColor) {
-	Ti.API.info("setNegativeColor()");
-	Ti.API.info("_negativeColor = "+_negativeColor); 
-	
-	negativeColor = _negativeColor;
-};
-
-$.setSize = function (_height, _width) {
-	Ti.API.info("setSize()");
-	Ti.API.info("_height = "+_height);
-	Ti.API.info("_width = "+_width); 
-	
-	$.tiChartVw.height = _height;
-	$.tiChartVw.width = _width;
-};
-
-function setData (data) {
-	Ti.API.info("setData()");
-	Ti.API.info("data = "+data);
+$.setData = function (_data) {
+	data = _data;
+	// Ti.API.info("setData()");
+	// Ti.API.info("data = "+data);
 	
 	var attrNameArray = [];
 	var attrNameArrayIndex=0;
 	
 	for (var attrName in data[0]) {
-	  Ti.API.info("data attribute name = "+attrName);
+	  // Ti.API.info("data attribute name = "+attrName);
 	  attrNameArray[attrNameArrayIndex++] = attrName;
 	}
 	
@@ -78,30 +73,35 @@ function setData (data) {
 	var hasNegative = false;
 	var rangeVal;
 	
-	_.each($.tiChartVw.children, function(view) {
-		$.tiChartVw.remove(view);
-	});
+	clearChartView();
 	
-	Ti.API.info("colors = "+colors);
-	//var colors = ["#00261C", "#168039", "#45BF55"];
+	// Ti.API.info("colors = "+colors);
 	var colorIndex = 0;
 	var numColors = colors.length;
 	
-	Ti.API.info("length data = "+dataLen);
-	Ti.API.info("max data = "+maxVal);
-	Ti.API.info("min data = "+minVal);
+	// Ti.API.info("length data = "+dataLen);
+	// Ti.API.info("max data = "+maxVal);
+	// Ti.API.info("min data = "+minVal);
+	
+	if(chartTitle) {
+		$.chartTitleLbl.text = chartTitle;
+		$.chartTitleLbl.height = Ti.UI.SIZE;
+	} else {
+		$.chartTitleLbl.text = null;
+		$.chartTitleLbl.height = "0";
+	}
 	
 	if(minVal<0) {
-		Ti.API.info("Has negative Vals");
+		// Ti.API.info("Has negative Vals");
 		hasNegative = true;
 		rangeVal = maxVal - minVal;
 	} else {
 		rangeVal = maxVal;
 	}
 	
-	Ti.API.info("range = "+rangeVal);
+	// Ti.API.info("range = "+rangeVal);
 	
-	Ti.API.info("max data (after check) = "+maxVal);
+	// Ti.API.info("max data (after check) = "+maxVal);
 
 	var barWidth = 100/dataLen;
 	var barWidthFill = 0;
@@ -110,48 +110,27 @@ function setData (data) {
 		barWidth = 100/((2*dataLen)-1);
 	}
 	
-	Ti.API.info("barWidth = "+barWidth);
-	
-	var barChartVw = Ti.UI.createView({
-		layout: "horizontal",
-		width: Ti.UI.FILL,
-		height: Ti.UI.FILL,
-	});
+	// Ti.API.info("barWidth = "+barWidth);
 	
 	if(legendOn){
-		var barChartContVw = Ti.UI.createView({
-			layout: "horizontal",
-			width: Ti.UI.FILL,
-			height: "80%",
-		});
-		var barChartYLegendVw = Ti.UI.createView({
-			width: "20%",
-			height: Ti.UI.FILL,
-		});
-		var barChartXLegendVw = Ti.UI.createView({
-			width: "80%",
-			height: "20%",
-			right: "0",
-			top: "0"
-		});
-		
-		barChartContVw.add(barChartYLegendVw);
-		barChartContVw.add(barChartVw);
-		$.tiChartVw.add(barChartContVw);
-		$.tiChartVw.add(barChartXLegendVw);
-		
+		$.barChartYLegendVw.width = "20%";
+		$.barChartXLegendVw.height = "20%";
+		$.barChartContVw.height = "80%";
 	} else {
-		$.tiChartVw.add(barChartVw);
+		$.barChartYLegendVw.width = "0%";
+		$.barChartXLegendVw.height = "0%";
+		$.barChartContVw.height = "100%";
 	}
 	
 	for(var i=0;i<dataLen;i++)
 	{
-		//var barHeight = 100*data[i]/rangeVal;
 		var barHeight = 100*data[i][attrNameArray[1]]/rangeVal;
 		
-		Ti.API.info("data{i}% = "+barHeight);
+		// Ti.API.info("data{i}% = "+barHeight);
 		
-		if(i>dataLen-5){
+		// I have to do this to make sure there is enough space for the bars, otherwise, the last one or two are not shown
+		// 5 was working well but maybe 2 is OK also as it helps the bars reach the RHS better
+		if(i>dataLen-2){
 			if(spacersOn){
 				barWidth = (100/(2*dataLen)) - 1;
 			} else {
@@ -169,22 +148,22 @@ function setData (data) {
 		
 		
 		if(hasNegative){
-			Ti.API.info("Has negative check true");
+			// Ti.API.info("Has negative check true");
 			if(data[i][attrNameArray[1]]>=0){
-				Ti.API.info("Data val positive check true");
+				// Ti.API.info("Data val positive check true");
 				barVw.bottom = (-100*minVal/rangeVal).toString()+"%";
 			} else {
-				Ti.API.info("Data val positive check false");
+				// Ti.API.info("Data val positive check false");
 				barVw.backgroundColor = negativeColor;
 				barVw.bottom = (-100*(minVal-data[i][attrNameArray[1]])/rangeVal).toString()+"%";
 				barVw.height = (-barHeight).toString()+"%";	
 			}
 		}
 		
-		Ti.API.info("barVw.bottom = "+barVw.bottom);
+		// Ti.API.info("barVw.bottom = "+barVw.bottom);
 		
 		barWidthFill += barWidth;
-		barChartVw.add(barVw);
+		$.barChartVw.add(barVw);
 		
 		if((spacersOn) && (i<dataLen-1)) {
 			var barSpacerVw = Ti.UI.createView({
@@ -193,66 +172,160 @@ function setData (data) {
 				bottom: "0",
 				backgroundColor: "transparent"
 			});
-			barChartVw.add(barSpacerVw);	
+			$.barChartVw.add(barSpacerVw);	
 		}
 	}
 	
 	if(legendOn){
-		var minYLbl = Ti.UI.createLabel({
-			font: {fontSize: _fontSize},
+		var minYLbl = Alloy.createWidget('com.leorbrenman.tismallbarchart', 'axisLabel', {
 			text: minVal,
 			bottom: "0",
 			right: "5",
 			color: labelColor
-		});
+		}).getView();
 		
-		var midYLbl = Ti.UI.createLabel({
-			font: {fontSize: _fontSize},
-			text: maxVal/2,
-			bottom: "50%",
+		var midYLbl = Alloy.createWidget('com.leorbrenman.tismallbarchart', 'axisLabel', {
+			text: hasNegative ? "0" : maxVal/2,
+			bottom: hasNegative ? (-100*minVal/rangeVal).toString()+"%" : "50%",
 			right: "5",
 			color: labelColor
-		});
+		}).getView();
 		
-		if(hasNegative){
-			midYLbl.bottom = (-100*minVal/rangeVal).toString()+"%";
-			midYLbl.text = "0";
-		}
-		
-		var maxYLbl = Ti.UI.createLabel({
-			font: {fontSize: _fontSize},
+		var maxYLbl = Alloy.createWidget('com.leorbrenman.tismallbarchart', 'axisLabel', {
 			text: maxVal,
 			top: "0",
 			right: "5",
 			color: labelColor
-		});
+		}).getView();
 		
-		barChartYLegendVw.add(minYLbl);
-		barChartYLegendVw.add(maxYLbl);
-		barChartYLegendVw.add(midYLbl);
+		$.barChartYLegendVw.add(minYLbl);
+		$.barChartYLegendVw.add(maxYLbl);
+		$.barChartYLegendVw.add(midYLbl);
 		
-		var minXLbl = Ti.UI.createLabel({
-			font: {fontSize: _fontSize},
-			//text: "first",
+		var minXLbl = Alloy.createWidget('com.leorbrenman.tismallbarchart', 'axisLabel', {
 			text: data[0][attrNameArray[0]],
 			top: "0",
 			left: "5",
 			color: labelColor
-		});
+		}).getView();
 		
-		var maxXLbl = Ti.UI.createLabel({
-			font: {fontSize: _fontSize},
-			//text: "last",
+		var maxXLbl = Alloy.createWidget('com.leorbrenman.tismallbarchart', 'axisLabel', {
 			text: data[dataLen-1][attrNameArray[0]],
 			top: "0",
 			right: "5",
 			color: labelColor
-		});
+		}).getView();
 		
-		barChartXLegendVw.add(minXLbl);
-		barChartXLegendVw.add(maxXLbl);
+		$.barChartXLegendVw.add(minXLbl);
+		$.barChartXLegendVw.add(maxXLbl);
 	}
 	
-	Ti.API.info("TotalWidth% = "+barWidthFill);
+	// Ti.API.info("TotalWidth% = "+barWidthFill);
 };
-exports.setData = setData;
+
+$.setLegendOnStatus = function (_legendOnStatus) {
+	// Ti.API.info("setLegendOnStatus()");
+	// Ti.API.info("_legendOnStatus = "+_legendOnStatus); 
+	
+	legendOn = _legendOnStatus;
+	$.setData(data);
+};
+
+$.getLegendOnStatus = function () {
+	// Ti.API.info("getLegendOnStatus()"); 
+	
+	return legendOn;
+};
+
+$.setSpacersOnStatus = function (_spacersOnStatus) {
+	// Ti.API.info("setSpacersOnStatus()");
+	// Ti.API.info("_spacersOnStatus = "+_spacersOnStatus); 
+	
+	spacersOn = _spacersOnStatus;
+	$.setData(data);
+};
+
+$.getSpacersOnStatus = function () {
+	// Ti.API.info("getSpacersOnStatus()"); 
+	
+	return spacersOn;
+};
+
+$.setColors = function (_colors) {
+	// Ti.API.info("setColors()");
+	// Ti.API.info("_colors = "+_colors); 
+	
+	// process color as string or array of strings
+	if(_colors) {
+		if(typeof _colors === "string") {
+			colors = [_colors];
+		}  else {
+			colors = _colors;
+		}
+	} else {
+		colors = defaultColors;
+	}
+	
+	//colors = _colors;
+	$.setData(data);
+};
+
+$.getColors = function () {
+	// Ti.API.info("getColors()"); 
+	
+	return colors;
+};
+
+$.setChartTitle = function (_title) {
+	// Ti.API.info("setChartTitle()");
+	// Ti.API.info("_title = "+_title); 
+	
+	chartTitle = _title;
+	$.setData(data);
+};
+
+$.getChartTitle = function () {
+	// Ti.API.info("getChartTitle()"); 
+	
+	return chartTitle;
+};
+
+$.clearChartTitle = function () {
+	// Ti.API.info("clearChartTitle()"); 
+	
+	chartTitle = null;
+	$.setData(data);
+};
+
+$.setNegativeColor = function (_negativeColor) {
+	// Ti.API.info("setNegativeColor()");
+	// Ti.API.info("_negativeColor = "+_negativeColor); 
+	
+	negativeColor = _negativeColor;
+	$.setData(data);
+};
+
+$.getNegativeColor = function () {
+	// Ti.API.info("getNegativeColor()"); 
+	
+	return negativeColor;
+};
+
+$.setSize = function (_height, _width) {
+	// Ti.API.info("setSize()");
+	// Ti.API.info("_height = "+_height);
+	// Ti.API.info("_width = "+_width); 
+	
+	$.tiChartVw.height = _height;
+	$.tiChartVw.width = _width;
+	$.setData(data);
+};
+
+$.getSize = function () {
+	// Ti.API.info("getSize()");
+
+	var size = {height: $.tiChartVw.height, width: $.tiChartVw.width}; 
+	return size;
+};
+
+$.setData(data);
